2b3bcff90539223c76aac60e30ce161903e60360
Evgenii Kolesnikov <jekow@yandex.ru>
2025-03-13 04:49:35 +0300

    add .editorconfig

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMover : MonoBehaviour
-{
+public class PlayerMover : MonoBehaviour {
     private Rigidbody2D _rb;
 
     [SerializeField] float _speedX = 1;
@@ -16,35 +15,28 @@ public class PlayerMover : MonoBehaviour
     private bool _isJump;
     private bool _isGround;
 
-    void Start()
-    {
+    void Start() {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
+    void Update() {
         _direction = Input.GetAxis(HORIZONTAL_AXIS);
-        if (_isGround && Input.GetKeyDown(KeyCode.W))
-        {
+        if (_isGround && Input.GetKeyDown(KeyCode.W)) {
             _isJump = true;
         }
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         _rb.velocity = new Vector2(_speedX * _direction * SPEED_COEFFICIENT * Time.fixedDeltaTime, _rb.velocity.y);
-        if (_isJump)
-        {
+        if (_isJump) {
             _rb.AddForce(new Vector2(0, _jumpForce));
             _isJump = false;
             _isGround = false;
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag(GROUND_TAG))
-        {
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag(GROUND_TAG)) {
             _isGround = true;
         }
     }
