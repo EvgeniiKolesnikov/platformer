afa1f51ca833703fe20cd2e2958125424baa0cfd
Evgenii Kolesnikov <jekow@yandex.ru>
2025-05-14 20:47:30 +0300

    Refactor PlayerMover.cs

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,36 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMover : MonoBehaviour {
-    private Rigidbody2D _rb;
-
-    [SerializeField] float _speed = 1;
-    [SerializeField] float _dashForce = 2000;
-    private const float SPEED_COEFF = 50;
+public class PlayerMover : MonoBehaviour
+{
     private const string HORIZONTAL_AXIS = "Horizontal";
     private const string VERTICAL_AXIS = "Vertical";
+    private const float SPEED_COEFF = 1;
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _dashForce = 200;
+
+    private Rigidbody2D _rigidbody;
     private Vector3 _direction;
+    private float _directionX;
+    private float _directionY;
     private bool _isDash;
 
-    void Awake() {
-        _rb = GetComponent<Rigidbody2D>();
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
-        _direction = new Vector2(Input.GetAxis(HORIZONTAL_AXIS), Input.GetAxis(VERTICAL_AXIS));
+    private void Update()
+    {
+        _directionX = Input.GetAxis(HORIZONTAL_AXIS);
+        _directionY = Input.GetAxis(VERTICAL_AXIS);
 
-        if (_direction != Vector3.zero) {
-            transform.up = -_direction.normalized;
-        }
-        if (!_isDash && Input.GetKeyDown(KeyCode.Space)) {
+        _direction = new Vector2(_directionX, _directionY).normalized;
+
+        // if (_direction != Vector3.zero)
+        // {
+        //     transform.up = -_direction.normalized;
+        // }
+        if (!_isDash && Input.GetKeyDown(KeyCode.Space))
+        {
             _isDash = true;
         }
     }
 
-    void FixedUpdate() {
-        _rb.velocity = _speed * SPEED_COEFF * Time.fixedDeltaTime * _direction;
-        if (_isDash) {
-            _rb.AddForce(_direction * _dashForce);
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _direction * _speed * SPEED_COEFF;
+        if (_isDash)
+        {
+            _rigidbody.AddForce(_direction * _dashForce);
             _isDash = false;
         }
     }
